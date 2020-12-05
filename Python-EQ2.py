1f1b7747d2d6e2cc05f8823ed9d72a345873b6f7
DeltΔ <75542302+Delta-repo@users.noreply.github.com>
2020-12-05 20:07:05 +0100

    Add files via upload

---
+++ Python-EQ2.py
@@ -0,0 +1,11 @@
+print('Welcome to the Delta Tool.')
+
+print('[1] - create (réer une base de données locale)')
+print('[1] - write (Mettre une information dans une database)')
+
+d = input('>')
+
+if d == '1':
+    
+
+if d == '1':
