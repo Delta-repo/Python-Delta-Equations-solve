c7cf5c924b42ad3617e22e1f19e72bc64bc401d4
DeltΔ <75542302+Delta-repo@users.noreply.github.com>
2020-12-05 20:07:54 +0100

    Update Python-EQ2.py

--- Python-EQ2.py
+++ Python-EQ2.py
@@ -1,11 +1,31 @@
-print('Welcome to the Delta Tool.')
+import os
+import math
 
-print('[1] - create (réer une base de données locale)')
-print('[1] - write (Mettre une information dans une database)')
+print('Calculer les fonctions du premier degré:')
 
-d = input('>')
+a = float(input("A (0x2) = "))
+b = float(input("B (0x)= "))
+c = float(input("C (0)= "))
 
-if d == '1':
-    
+clear = lambda: os.system('cls')
+clear()
 
-if d == '1':
+
+print('Pour trouver Delta : ',b,'2 - 4.',a,'.',c)
+d = b**2 - 4*a*c
+
+print('Delta est égale à : ' ,d,)
+
+if(d < 0):
+    print("L'équation n'a pas de solution :") 
+else:
+    if (d == 0):
+        print("L'équation a une solution double :")
+        x = -b / (2.*a)
+        print ("La solution est x = ",x)
+    else:
+        print("L'équation a deux solutions solutions :")
+        x1 = (-b - math.sqrt(d)) /(2*a)
+        x2 = (-b + math.sqrt(d)) /(2*a)
+        print ("x1 = ",x1)
+        print ("x2 = ",x2)
